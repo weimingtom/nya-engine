ea6a426257ffa7fb6c0300426b4877993f2ca1cc
nya! <nyan.developer@gmail.com>
2012-06-04 02:45:05 +0400

    tsb animations

---
+++ tests/npl2/tsb_anim.cpp
@@ -0,0 +1,76 @@
+//https://code.google.com/p/nya-engine/
+
+#include "tsb_anim.h"
+#include "memory/tmp_buffer.h"
+#include <stdio.h>
+#include <string.h>
+
+bool tsb_anim::load(nya_resources::resource_data *anim_res)
+{
+    if(!anim_res)
+    {
+        nya_resources::get_log()<<"Unable to load animation: invalid data\n";
+        return false;
+    }
+
+    nya_memory::tmp_buffer_scoped anim_data(anim_res->get_size());
+    anim_res->read_all(anim_data.get_data());
+    anim_res->release();
+
+    typedef unsigned int uint;
+
+    struct tsb_header
+    {
+        const char magic[4];
+        uint pad[3];
+        uint bone_count;
+        uint frame_count;
+        uint unknown;
+        uint special_count;
+    };
+
+    tsb_header *header=(tsb_header*)anim_data.get_data();
+    if(strncmp(header->magic,"TSB0",4)!=0)
+    {
+        nya_resources::get_log()<<"Invalid TSB0 magic in animation file\n";
+        //return false;
+    }
+
+    m_bones_count=header->bone_count;
+    m_frames_count=header->frame_count;
+
+    uint size=header->bone_count*header->frame_count;
+    m_data.resize(size);
+    if(!anim_data.copy_from(&m_data[0],size*sizeof(bone),sizeof(tsb_header)))
+    {
+        nya_resources::get_log()<<"Unable to load tsb animation file\n";
+        return false;
+    }
+
+    return true;
+}
+
+bool shared_anims_manager::fill_resource(const char *name,tsb_anim &res)
+{
+    if(!name)
+    {
+        nya_resources::get_log()<<"Unable to access model: invalid name\n";
+        return false;
+    }
+
+    nya_resources::resource_data *data = nya_resources::get_resources_provider().access(name);
+
+    return res.load(data);
+}
+
+bool shared_anims_manager::release_resource(tsb_anim &res)
+{
+    res.release();
+    return true;
+}
+
+shared_anims_manager &get_anims_manager()
+{
+    static shared_anims_manager manager;
+    return manager;
+}
---
+++ tests/npl2/tsb_anim.h
@@ -0,0 +1,54 @@
+//https://code.google.com/p/nya-engine/
+
+#ifndef tsb_anim_h
+#define tsb_anim_h
+
+#include "resources/resources.h"
+#include "resources/shared_resources.h"
+#include <vector>
+
+class tsb_anim
+{
+public:
+    bool load(nya_resources::resource_data *data);
+    
+    struct bone
+    {
+        float mat[4][4];
+    };
+
+    bone *get_bones(unsigned int frame)
+    {
+        if(!m_bones_count || frame>=m_frames_count)
+            return 0;
+
+        return &m_data[m_bones_count*(frame % m_frames_count)];
+    }
+
+    unsigned int get_bones_count() { return m_bones_count; }
+    unsigned int get_frames_count() { return m_frames_count; }
+
+    void release() { m_data.clear(); m_bones_count=m_frames_count=0; }
+
+    tsb_anim(): m_bones_count(0), m_frames_count(0) {}
+
+private:
+    unsigned int m_bones_count;
+    unsigned int m_frames_count;
+
+private:
+    std::vector<bone> m_data;
+};
+
+typedef nya_resources::shared_resources<tsb_anim,8> shared_anims;
+typedef shared_anims::shared_resource_ref anim_ref;
+
+class shared_anims_manager: public shared_anims
+{
+    bool fill_resource(const char *name,tsb_anim &res);
+    bool release_resource(tsb_anim &res);
+};
+
+shared_anims_manager &get_anims_manager();
+
+#endif
