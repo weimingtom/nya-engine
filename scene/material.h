9396c35d103c3e4af340c779547b5dd4966c774c
nya! <nyan.developer@gmail.com>
2013-02-13 00:18:59 +0300

    material param set

--- scene/material.h
+++ scene/material.h
@@ -25,14 +25,14 @@ public:
         friend class material;
 
     public:
-        void set(float f0,float f1,float f2,float f3);
-        
-        param() { for(int i=0;i<4;++i) f[i]=0.0f; }
-        param(float f0,float f1,float f2,float f3)
+        void set(float f0,float f1,float f2,float f3)
         {
             f[0]=f0; f[1]=f1;
             f[2]=f2; f[3]=f3;
         }
+
+        param() { for(int i=0;i<4;++i) f[i]=0.0f; }
+        param(float f0,float f1,float f2,float f3) { set(f0,f1,f2,f3); }
 
     private:
         float f[4];
