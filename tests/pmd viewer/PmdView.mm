b6ae80753a4984c1293acb56d9a160b884c5b7ce
nya! <nyan.developer@gmail.com>
2013-03-09 03:09:29 +0300

    pmd viewer - pmx

--- tests/pmd viewer/PmdView.mm
+++ tests/pmd viewer/PmdView.mm
@@ -127,48 +127,152 @@ bool load_texture(nya_scene::shared_texture &res,nya_scene::resource_data &textu
     return true;
 }
 
-bool load_pmx(nya_scene::shared_mesh &res,nya_scene::resource_data &data,const char* name)
+class pmx_loader
 {
-    if(!data.get_size())
-        return false;
-    
-    nya_memory::memory_reader reader(data.get_data(),data.get_size());
-    if(!reader.test("PMX ",4))
-        return false;
-    
-    if(reader.read<float>()!=2.0f)
-        return false;
-
     struct pmx_header
     {
         char text_encoding;
         char extended_uv;
-        char vertex_index_size;
-        char texture_index_size;
-        char material_index_size;
-        char bone_index_size;
-        char morph_index_size;
-        char rigidbody_index_size;
+        char index_size;
+        char texture_idx_size;
+        char material_idx_size;
+        char bone_idx_size;
+        char morph_idx_size;
+        char rigidbody_idx_size;
     };
 
-    const char header_size=reader.read<char>();
-    if(header_size!=sizeof(pmx_header))
-        return false;
-
-    const pmx_header header=reader.read<pmx_header>();
-    if(header.extended_uv>0)
-        return false; //ToDo
-    
-    for(int i=0;i<4;++i)
+    struct vert
     {
-        const int size=reader.read<int>();
-        reader.skip(size);
+        float pos[3];
+        float normal[3];
+        float tc[2];
+        float bone_idx[4];
+        float bone_weight[4];
+    };
+
+    static float read_idx(nya_memory::memory_reader &reader,int size)
+    {
+        switch(size)
+        {
+            case 1: return (float)reader.read<unsigned char>();
+            case 2: return (float)reader.read<unsigned short>();
+            case 4: return (float)reader.read<int>();
+        }
+
+        return 0.0f;
     }
-    
-    const int vert_count=reader.read<int>();
-    
-    return true;
-}
+
+public:
+    static bool load(nya_scene::shared_mesh &res,nya_scene::resource_data &data,const char* name)
+    {
+        if(!data.get_size())
+            return false;
+        
+        nya_memory::memory_reader reader(data.get_data(),data.get_size());
+        if(!reader.test("PMX ",4))
+            return false;
+        
+        if(reader.read<float>()!=2.0f)
+            return false;
+
+        const char header_size=reader.read<char>();
+        if(header_size!=sizeof(pmx_header))
+            return false;
+
+        const pmx_header header=reader.read<pmx_header>();
+        if(header.extended_uv>0)
+            return false; //ToDo
+        
+        for(int i=0;i<4;++i)
+        {
+            const int size=reader.read<int>();
+            reader.skip(size);
+        }
+        
+        const int vert_count=reader.read<int>();
+
+        std::vector<vert> verts(vert_count);
+
+        for(int i=0;i<vert_count;++i)
+        {
+            vert &v=verts[i];
+
+            v.pos[0]=reader.read<float>();
+            v.pos[1]=reader.read<float>();
+            v.pos[2]=-reader.read<float>();
+
+            v.normal[0]=reader.read<float>();
+            v.normal[1]=reader.read<float>();
+            v.normal[2]=-reader.read<float>();
+
+            v.tc[0]=reader.read<float>();
+            v.tc[1]=1.0-reader.read<float>();
+
+            switch(reader.read<char>())
+            {
+                case 0:
+                    v.bone_idx[0]=read_idx(reader,header.bone_idx_size);
+                    v.bone_weight[0]=1.0f;
+                    for(int j=1;j<4;++j)
+                        v.bone_weight[j]=v.bone_idx[j]=0.0f;
+                break;
+                    
+                case 1:
+                    v.bone_idx[0]=read_idx(reader,header.bone_idx_size);
+                    v.bone_idx[1]=read_idx(reader,header.bone_idx_size);
+                    
+                    v.bone_weight[0]=reader.read<float>();
+                    v.bone_weight[1]=1.0f-v.bone_weight[0];
+                    
+                    for(int j=2;j<4;++j)
+                        v.bone_weight[j]=v.bone_idx[j]=0.0f;
+                    break;
+
+                case 2:
+                    for(int j=0;j<4;++j)
+                        v.bone_idx[j]=read_idx(reader,header.bone_idx_size);
+
+                    for(int j=0;j<4;++j)
+                        v.bone_weight[j]=reader.read<float>();
+                break;
+
+                case 3:
+                    v.bone_idx[0]=read_idx(reader,header.bone_idx_size);
+                    v.bone_idx[1]=read_idx(reader,header.bone_idx_size);
+                    
+                    v.bone_weight[0]=reader.read<float>();
+                    v.bone_weight[1]=1.0f-v.bone_weight[0];
+                    
+                    for(int j=2;j<4;++j)
+                        v.bone_weight[j]=v.bone_idx[j]=0.0f;
+
+                    reader.skip(sizeof(float)*3*3);
+                break;
+
+                default: return false;
+            }
+
+            reader.read<float>(); //edge
+        }
+
+        res.vbo.set_vertex_data(&verts[0],sizeof(vert),vert_count);
+        res.vbo.set_vertices(0,3);
+
+        const int indices_count=reader.read<int>();
+        if(header.index_size==2)
+            res.vbo.set_index_data(reader.get_data(),nya_render::vbo::index2b,indices_count);
+        else if(header.index_size==4)
+            res.vbo.set_index_data(reader.get_data(),nya_render::vbo::index4b,indices_count);
+        else
+            return false;
+
+        res.groups.resize(1);
+        res.groups[0].offset=0;
+        res.groups[0].count=indices_count;
+
+        return true;
+    }
+};
 
 @implementation PmdView
 
@@ -236,8 +340,9 @@ bool load_pmx(nya_scene::shared_mesh &res,nya_scene::resource_data &data,const c
         glClearColor(0.2,0.4,0.5,0);
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LESS);
-
+        
         nya_scene::texture::register_load_function(load_texture);
+        nya_scene::mesh::register_load_function(pmx_loader::load);
         m_mesh.load(doc->m_model_name.c_str());
 
         doc->m_model_name.clear();
