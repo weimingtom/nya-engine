71b74c1caead8d707b48dcb35dc31a6bc621ef62
nya! <nyan.developer@gmail.com>
2014-02-25 17:39:53 +0400

    render: removed unnecessary shader extensions

--- render/shader.cpp
+++ render/shader.cpp
@@ -185,32 +185,7 @@ namespace
     PFNGLGENPROGRAMSARBPROC glGenProgramsARB = NULL;
     PFNGLDELETEPROGRAMSARBPROC glDeleteProgramsARB = NULL;
     PFNGLBINDPROGRAMARBPROC glBindProgramARB = NULL;
-    PFNGLISPROGRAMARBPROC glIsProgramARB = NULL;
-
-    PFNGLPROGRAMSTRINGARBPROC glProgramStringARB = NULL;
-    PFNGLGETPROGRAMIVARBPROC glGetProgramivARB = NULL;
-
-    PFNGLVERTEXATTRIB4FARBPROC glVertexAttrib4fARB = NULL;
-    PFNGLVERTEXATTRIB4FVARBPROC glVertexAttrib4fvARB = NULL;
-    PFNGLVERTEXATTRIB3FARBPROC glVertexAttrib3fARB = NULL;
-    PFNGLVERTEXATTRIB3FVARBPROC glVertexAttrib3fvARB = NULL;
-
-    PFNGLVERTEXATTRIBPOINTERARBPROC glVertexAttribPointerARB = NULL;
-    PFNGLENABLEVERTEXATTRIBARRAYARBPROC glEnableVertexAttribArrayARB = NULL;
-    PFNGLDISABLEVERTEXATTRIBARRAYARBPROC glDisableVertexAttribArrayARB = NULL;
-
-    PFNGLPROGRAMLOCALPARAMETER4FARBPROC glProgramLocalParameter4fARB = NULL;
-    PFNGLPROGRAMLOCALPARAMETER4FVARBPROC glProgramLocalParameter4fvARB = NULL;
-
-    PFNGLGETPROGRAMLOCALPARAMETERFVARBPROC glGetProgramLocalParameterfvARB = NULL;
-
-    PFNGLPROGRAMENVPARAMETER4FARBPROC glProgramEnvParameter4fARB = NULL;
-    PFNGLPROGRAMENVPARAMETER4FVARBPROC glProgramEnvParameter4fvARB = NULL;
-
-    PFNGLGETPROGRAMENVPARAMETERFVARBPROC glGetProgramEnvParameterfvARB = NULL;
-
     PFNGLDELETEOBJECTARBPROC glDeleteObjectARB = NULL;
-    PFNGLGETHANDLEARBPROC glGetHandleARB = NULL;
     PFNGLDETACHOBJECTARBPROC glDetachObjectARB = NULL;
     PFNGLCREATESHADEROBJECTARBPROC glCreateShaderObjectARB = NULL;
     PFNGLSHADERSOURCEARBPROC glShaderSourceARB = NULL;
@@ -220,40 +195,15 @@ namespace
     PFNGLLINKPROGRAMARBPROC glLinkProgramARB = NULL;
     PFNGLUSEPROGRAMOBJECTARBPROC glUseProgramObjectARB = NULL;
     PFNGLVALIDATEPROGRAMARBPROC glValidateProgramARB = NULL;
-    PFNGLUNIFORM1FARBPROC glUniform1fARB = NULL;
-    PFNGLUNIFORM2FARBPROC glUniform2fARB = NULL;
-    PFNGLUNIFORM3FARBPROC glUniform3fARB = NULL;
     PFNGLUNIFORM4FARBPROC glUniform4fARB = NULL;
     PFNGLUNIFORM1IARBPROC glUniform1iARB = NULL;
-    PFNGLUNIFORM2IARBPROC glUniform2iARB = NULL;
-    PFNGLUNIFORM3IARBPROC glUniform3iARB = NULL;
-    PFNGLUNIFORM4IARBPROC glUniform4iARB = NULL;
-    PFNGLUNIFORM1FVARBPROC glUniform1fvARB = NULL;
-    PFNGLUNIFORM2FVARBPROC glUniform2fvARB = NULL;
     PFNGLUNIFORM3FVARBPROC glUniform3fvARB = NULL;
     PFNGLUNIFORM4FVARBPROC glUniform4fvARB = NULL;
-    PFNGLUNIFORM1IVARBPROC glUniform1ivARB = NULL;
-    PFNGLUNIFORM2IVARBPROC glUniform2ivARB = NULL;
-    PFNGLUNIFORM3IVARBPROC glUniform3ivARB = NULL;
-    PFNGLUNIFORM4IVARBPROC glUniform4ivARB = NULL;
-    PFNGLUNIFORMMATRIX2FVARBPROC glUniformMatrix2fvARB = NULL;
-    PFNGLUNIFORMMATRIX3FVARBPROC glUniformMatrix3fvARB = NULL;
     PFNGLUNIFORMMATRIX4FVARBPROC glUniformMatrix4fvARB = NULL;
-    PFNGLGETOBJECTPARAMETERFVARBPROC glGetObjectParameterfvARB = NULL;
     PFNGLGETOBJECTPARAMETERIVARBPROC glGetObjectParameterivARB = NULL;
     PFNGLGETINFOLOGARBPROC glGetInfoLogARB = NULL;
-    PFNGLGETATTACHEDOBJECTSARBPROC glGetAttachedObjectsARB = NULL;
     PFNGLGETUNIFORMLOCATIONARBPROC glGetUniformLocationARB = NULL;
-    PFNGLGETACTIVEUNIFORMARBPROC glGetActiveUniformARB = NULL;
-    PFNGLGETUNIFORMFVARBPROC glGetUniformfvARB = NULL;
-    PFNGLGETUNIFORMIVARBPROC glGetUniformivARB = NULL;
-    PFNGLGETSHADERSOURCEARBPROC glGetShaderSourceARB = NULL;
-
-    PFNGLBINDATTRIBLOCATIONARBPROC glBindAttribLocationARB = NULL;
-    PFNGLGETACTIVEATTRIBARBPROC glGetActiveAttribARB = NULL;
-    PFNGLGETATTRIBLOCATIONARBPROC glGetAttribLocationARB = NULL;
-    PFNGLGETVERTEXATTRIBFVARBPROC glGetVertexAttribfvARB = NULL;
-  #endif
+#endif
 
   #ifdef OPENGL_ES
     #define glCreateProgramObjectARB glCreateProgram
@@ -314,24 +264,7 @@ bool check_init_shaders()
 
     glDeleteProgramsARB             = (PFNGLDELETEPROGRAMSARBPROC)            get_extension ( "glDeleteProgramsARB" );
     glBindProgramARB                = (PFNGLBINDPROGRAMARBPROC)               get_extension ( "glBindProgramARB" );
-    glIsProgramARB                  = (PFNGLISPROGRAMARBPROC)                 get_extension ( "glIsProgramARB" );
-    glProgramStringARB              = (PFNGLPROGRAMSTRINGARBPROC)             get_extension ( "glProgramStringARB" );
-    glGetProgramivARB               = (PFNGLGETPROGRAMIVARBPROC)              get_extension ( "glGetProgramivARB" );
-    glVertexAttrib4fARB             = (PFNGLVERTEXATTRIB4FARBPROC)            get_extension ( "glVertexAttrib4fARB" );
-    glVertexAttrib4fvARB            = (PFNGLVERTEXATTRIB4FVARBPROC)           get_extension ( "glVertexAttrib4fvARB" );
-    glVertexAttrib3fARB             = (PFNGLVERTEXATTRIB3FARBPROC)            get_extension ( "glVertexAttrib3fARB" );
-    glVertexAttrib3fvARB            = (PFNGLVERTEXATTRIB3FVARBPROC)           get_extension ( "glVertexAttrib3fvARB" );
-    glVertexAttribPointerARB        = (PFNGLVERTEXATTRIBPOINTERARBPROC)       get_extension ( "glVertexAttribPointerARB" );
-    glEnableVertexAttribArrayARB    = (PFNGLENABLEVERTEXATTRIBARRAYARBPROC)   get_extension ( "glEnableVertexAttribArrayARB" );
-    glDisableVertexAttribArrayARB   = (PFNGLDISABLEVERTEXATTRIBARRAYARBPROC)  get_extension ( "glDisableVertexAttribArrayARB" );
-    glProgramLocalParameter4fARB    = (PFNGLPROGRAMLOCALPARAMETER4FARBPROC)   get_extension ( "glProgramLocalParameter4fARB" );
-    glProgramLocalParameter4fvARB   = (PFNGLPROGRAMLOCALPARAMETER4FVARBPROC)  get_extension ( "glProgramLocalParameter4fvARB" );
-    glGetProgramLocalParameterfvARB = (PFNGLGETPROGRAMLOCALPARAMETERFVARBPROC)get_extension ( "glGetProgramLocalParameterfvARB" );
-    glProgramEnvParameter4fARB      = (PFNGLPROGRAMENVPARAMETER4FARBPROC)     get_extension ( "glProgramEnvParameter4fARB" );
-    glProgramEnvParameter4fvARB     = (PFNGLPROGRAMENVPARAMETER4FVARBPROC)    get_extension ( "glProgramEnvParameter4fvARB" );
-    glGetProgramEnvParameterfvARB   = (PFNGLGETPROGRAMENVPARAMETERFVARBPROC)  get_extension ( "glGetProgramEnvParameterfvARB" );
     glDeleteObjectARB         = (PFNGLDELETEOBJECTARBPROC)         get_extension ( "glDeleteObjectARB" );
-    glGetHandleARB            = (PFNGLGETHANDLEARBPROC)            get_extension ( "glGetHandleARB" );
     glDetachObjectARB         = (PFNGLDETACHOBJECTARBPROC)         get_extension ( "glDetachObjectARB" );
     glCreateShaderObjectARB   = (PFNGLCREATESHADEROBJECTARBPROC)   get_extension ( "glCreateShaderObjectARB" );
     glShaderSourceARB         = (PFNGLSHADERSOURCEARBPROC)         get_extension ( "glShaderSourceARB" );
@@ -341,38 +274,16 @@ bool check_init_shaders()
     glLinkProgramARB          = (PFNGLLINKPROGRAMARBPROC)          get_extension ( "glLinkProgramARB" );
     glUseProgramObjectARB     = (PFNGLUSEPROGRAMOBJECTARBPROC)     get_extension ( "glUseProgramObjectARB" );
     glValidateProgramARB      = (PFNGLVALIDATEPROGRAMARBPROC)      get_extension ( "glValidateProgramARB" );
-    glUniform1fARB            = (PFNGLUNIFORM1FARBPROC)            get_extension ( "glUniform1fARB" );
-    glUniform2fARB            = (PFNGLUNIFORM2FARBPROC)            get_extension ( "glUniform2fARB" );
-    glUniform3fARB            = (PFNGLUNIFORM3FARBPROC)            get_extension ( "glUniform3fARB" );
     glUniform4fARB            = (PFNGLUNIFORM4FARBPROC)            get_extension ( "glUniform4fARB" );
     glUniform1iARB            = (PFNGLUNIFORM1IARBPROC)            get_extension ( "glUniform1iARB" );
-    glUniform2iARB            = (PFNGLUNIFORM2IARBPROC)            get_extension ( "glUniform2iARB" );
-    glUniform3iARB            = (PFNGLUNIFORM3IARBPROC)            get_extension ( "glUniform3iARB" );
-    glUniform4iARB            = (PFNGLUNIFORM4IARBPROC)            get_extension ( "glUniform4iARB" );
-    glUniform1fvARB           = (PFNGLUNIFORM1FVARBPROC)           get_extension ( "glUniform1fvARB" );
-    glUniform2fvARB           = (PFNGLUNIFORM2FVARBPROC)           get_extension ( "glUniform2fvARB" );
     glUniform3fvARB           = (PFNGLUNIFORM3FVARBPROC)           get_extension ( "glUniform3fvARB" );
     glUniform4fvARB           = (PFNGLUNIFORM4FVARBPROC)           get_extension ( "glUniform4fvARB" );
-    glUniform1ivARB           = (PFNGLUNIFORM1IVARBPROC)           get_extension ( "glUniform1ivARB" );
-    glUniform2ivARB           = (PFNGLUNIFORM2IVARBPROC)           get_extension ( "glUniform2ivARB" );
-    glUniform3ivARB           = (PFNGLUNIFORM3IVARBPROC)           get_extension ( "glUniform3ivARB" );
-    glUniform4ivARB           = (PFNGLUNIFORM4IVARBPROC)           get_extension ( "glUniform4ivARB" );
     glUniformMatrix2fvARB     = (PFNGLUNIFORMMATRIX2FVARBPROC)     get_extension ( "glUniformMatrix2fvARB" );
     glUniformMatrix3fvARB     = (PFNGLUNIFORMMATRIX3FVARBPROC)     get_extension ( "glUniformMatrix3fvARB" );
     glUniformMatrix4fvARB     = (PFNGLUNIFORMMATRIX4FVARBPROC)     get_extension ( "glUniformMatrix4fvARB" );
-    glGetObjectParameterfvARB = (PFNGLGETOBJECTPARAMETERFVARBPROC) get_extension ( "glGetObjectParameterfvARB" );
     glGetObjectParameterivARB = (PFNGLGETOBJECTPARAMETERIVARBPROC) get_extension ( "glGetObjectParameterivARB" );
     glGetInfoLogARB           = (PFNGLGETINFOLOGARBPROC)           get_extension ( "glGetInfoLogARB" );
-    glGetAttachedObjectsARB   = (PFNGLGETATTACHEDOBJECTSARBPROC)   get_extension ( "glGetAttachedObjectsARB" );
     glGetUniformLocationARB   = (PFNGLGETUNIFORMLOCATIONARBPROC)   get_extension ( "glGetUniformLocationARB" );
-    glGetActiveUniformARB     = (PFNGLGETACTIVEUNIFORMARBPROC)     get_extension ( "glGetActiveUniformARB" );
-    glGetUniformfvARB         = (PFNGLGETUNIFORMFVARBPROC)         get_extension ( "glGetUniformfvARB" );
-    glGetUniformivARB         = (PFNGLGETUNIFORMIVARBPROC)         get_extension ( "glGetUniformivARB" );
-    glGetShaderSourceARB      = (PFNGLGETSHADERSOURCEARBPROC)      get_extension ( "glGetShaderSourceARB" );
-    glBindAttribLocationARB   = (PFNGLBINDATTRIBLOCATIONARBPROC)   get_extension ( "glBindAttribLocationARB" );
-    glGetActiveAttribARB      = (PFNGLGETACTIVEATTRIBARBPROC)      get_extension ( "glGetActiveAttribARB"    );
-    glGetAttribLocationARB    = (PFNGLGETATTRIBLOCATIONARBPROC)    get_extension ( "glGetAttribLocationARB"  );
-    glGetVertexAttribfvARB    = (PFNGLGETVERTEXATTRIBFVARBPROC)    get_extension ( "glGetVertexAttribfvARB"  );
   #endif
     failed=false;
     initialised=true;
