ea50553ab6033f99147015abb38a5d71a0ac7e4c
nya! <nyan.developer@gmail.com>
2014-04-16 18:15:26 +0400

    dds load fix

--- formats/dds.cpp
+++ formats/dds.cpp
@@ -102,9 +102,9 @@ size_t dds::decode_header(const void *data,size_t size)
         else
             return 0;
 
-        this->mip0_data_size=width*height*pf.bpp/4;
-        for(uint i=0,s=uint(mip0_data_size);i<mipmap_count;++i,s/=4)
-            this->data_size+=s;
+        this->mip0_data_size=width*height*(pf.bpp/4);
+        for(uint i=0,w=width,h=height;i<mipmap_count;++i,w>1?w/=2:1,h>1?h/=2:1)
+            this->data_size+=w*h*(pf.bpp/4);
     }
     else
         return 0;
