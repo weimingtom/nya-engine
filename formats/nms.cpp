7387e967d386e6f282993993adea35d817ab1d35
nya <nyan.developer@gmail.com>
2014-07-07 20:44:47 +0400

    nms mesh loading code is migrating to formats

---
+++ formats/nms.cpp
@@ -0,0 +1,107 @@
+//https://code.google.com/p/nya-engine/
+
+#include "nms.h"
+#include "memory/memory_reader.h"
+#include <stdio.h>
+#include <stdint.h>
+
+namespace nya_formats
+{
+
+static std::string read_string(nya_memory::memory_reader &reader)
+{
+    unsigned short size=reader.read<unsigned short>();
+    const char *str=(const char *)reader.get_data();
+    if(!size || !str || !reader.check_remained(size))
+    {
+        reader.skip(size);
+        return "";
+    }
+
+    reader.skip(size);
+
+    return std::string(str,size);
+}
+
+bool nms::read_chunks_info(const void *data,size_t size)
+{
+    *this=nms();
+
+    if(!data || !size)
+        return false;
+
+    nya_memory::memory_reader reader(data,size);
+    if(!reader.test("nya mesh",8))
+        return false;
+
+    version=reader.read<uint32_t>();
+
+    chunks.resize(reader.read<uint32_t>());
+    for(size_t i=0;i<chunks.size();++i)
+    {
+        chunk_info &c=chunks[i];
+        c.type=reader.read<uint32_t>();
+        c.size=reader.read<uint32_t>();
+        c.data=reader.get_data();
+
+        if(!reader.check_remained(c.size))
+        {
+            *this=nms();
+            return false;
+        }
+
+        reader.skip(c.size);
+    }
+
+    return true;
+}
+
+bool nms_material_chunk::read(const void *data,size_t size,int version)
+{
+    *this=nms_material_chunk();
+
+    if(!data || !size)
+        return false;
+
+    nya_memory::memory_reader reader(data,size);
+
+    materials.resize(reader.read<uint16_t>());
+    for(size_t i=0;i<materials.size();++i)
+    {
+        material_info &m=materials[i];
+        m.name=read_string(reader);
+
+        m.textures.resize(reader.read<uint16_t>());
+        for(size_t j=0;j<m.textures.size();++j)
+        {
+            m.textures[j].semantics=read_string(reader);
+            m.textures[j].filename=read_string(reader);
+        }
+
+        m.strings.resize(reader.read<uint16_t>());
+        for(size_t j=0;j<m.strings.size();++j)
+        {
+            m.strings[j].name=read_string(reader);
+            m.strings[j].value=read_string(reader);
+        }
+
+        m.vectors.resize(reader.read<uint16_t>());
+        for(size_t j=0;j<m.vectors.size();++j)
+        {
+            m.vectors[j].name=read_string(reader);
+            for(int k=0;k<4;++k)
+                m.vectors[j].value[k]=reader.read<float>();
+        }
+
+        m.ints.resize(reader.read<uint16_t>());
+        for(size_t j=0;j<m.ints.size();++j)
+        {
+            m.ints[j].name=read_string(reader);
+            m.ints[j].value=reader.read<int32_t>();
+        }
+    }
+
+    return true;
+}
+
+}
---
+++ formats/nms.h
@@ -0,0 +1,93 @@
+//https://code.google.com/p/nya-engine/
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include <stddef.h>
+
+namespace nya_formats
+{
+
+struct nms
+{
+    unsigned int version;
+
+    struct chunk_info
+    {
+        unsigned int type;
+        unsigned int size;
+        const void *data;
+    };
+
+    enum section_type
+    {
+        mesh_data,
+        skeleton,
+        materials
+    };
+
+    std::vector<chunk_info> chunks;
+
+public:
+    bool read_chunks_info(const void *data,size_t size);
+
+public:
+    size_t get_nms_size();
+    size_t encode_nms(void *to_data,size_t to_size);
+};
+
+struct nms_mesh_chunk
+{
+public:
+    size_t read_header(const void *data,size_t size,int version); //0 if invalid
+};
+
+struct nms_material_chunk
+{
+    struct texture_info
+    {
+        std::string semantics;
+        std::string filename;
+    };
+
+    struct string_param
+    {
+        std::string name;
+        std::string value;
+    };
+
+    struct vector_param
+    {
+        std::string name;
+        float value[4];
+    };
+
+    struct int_param
+    {
+        std::string name;
+        int value;
+    };
+
+    struct material_info
+    {
+        std::string name;
+        std::vector<texture_info> textures;
+        std::vector<string_param> strings;
+        std::vector<vector_param> vectors;
+        std::vector<int_param> ints;
+    };
+
+    std::vector<material_info> materials;
+
+public:
+    bool read(const void *data,size_t size,int version);
+};
+
+struct nms_skeleton_chunk
+{
+public:
+    bool read(const void *data,size_t size,int version);
+};
+
+}
