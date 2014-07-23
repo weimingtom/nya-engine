e3a4d9e04f99a08a606514f91965c64d3b7a2da9
nya <nyan.developer@gmail.com>
2014-07-23 17:18:09 +0400

    mesh fix

--- scene/mesh.cpp
+++ scene/mesh.cpp
@@ -277,7 +277,7 @@ int mesh_internal::get_mat_idx(int group_idx) const
     if(!m_shared.is_valid())
         return -1;
 
-    if(group_idx<0)
+    if(group_idx<0 || group_idx>=(int)m_shared->groups.size())
         return -1;
 
     if(group_idx<(int)m_replaced_materials_idx.size())
