7146c665f203af7bf56b4fb76bec60e7223387cb
v000gd <->
2014-07-29 14:46:28 +0400

    Dds multichar char removed

--- formats/string_convert.h
+++ formats/string_convert.h
@@ -9,14 +9,21 @@
 namespace nya_formats
 {
 
+const char vector_string_delimeter = ',';
+const char blend_string_delimeter = ':';
+
 bool bool_from_string(const std::string &s);
 std::string string_from_bool(bool value);
+
 nya_math::vec4 vec4_from_string(const std::string &s);
 std::string string_from_vec4(const nya_math::vec4 &value,int precision = -1);
+
 nya_render::blend::mode blend_mode_from_string(const std::string &s);
 std::string string_from_blend_mode(nya_render::blend::mode value);
+
 bool blend_mode_from_string(const std::string &s,nya_render::blend::mode &src_out,nya_render::blend::mode &dst_out);
 std::string string_from_blend_mode(bool blend,nya_render::blend::mode src,nya_render::blend::mode dst);
+
 bool cull_face_from_string(const std::string &s,nya_render::cull_face::order &order_out);
 std::string string_from_cull_face(bool cull,nya_render::cull_face::order order);
 
