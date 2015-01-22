f07e6498ffddb69bf9c57060498041f78041b466
nya <nyan.developer@gmail.com>
2015-01-22 20:42:18 +0300

    ...

---
+++ math/scalar.h
@@ -0,0 +1,20 @@
+//https://code.google.com/p/nya-engine/
+
+#pragma once
+
+#ifdef min
+    #undef min
+#endif
+
+#ifdef max
+    #undef max
+#endif
+
+namespace nya_math
+{
+
+inline float max(float a,float b) { return a>b?a:b; }
+inline float min(float a,float b) { return a<b?a:b; }
+inline float clamp(float value,float from,float to) { return value<from?from:value>to?to:value; }
+
+}
