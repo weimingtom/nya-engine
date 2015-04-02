d0dfa5d899ded01985c0605663847fa019f011b0
nya! <nyan.developer@gmail.com>
2015-04-02 17:09:30 +0300

    ...

--- math/scalar.h
+++ math/scalar.h
@@ -16,5 +16,6 @@ namespace nya_math
 inline float max(float a,float b) { return a>b?a:b; }
 inline float min(float a,float b) { return a<b?a:b; }
 inline float clamp(float value,float from,float to) { return value<from?from:value>to?to:value; }
+inline float lerp(float from,float to,float t) { return from*(1.0f-t)+to*t; }
 
 }
